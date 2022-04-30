2b36fa7443baa8481a7b9afb9b2764075bbcb549
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-04-30 08:14:25 -0300

    :construction: adds get route to list all users as admin

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -49,7 +49,7 @@ class UsersRepository implements IUsersRepository {
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user || !user.admin) {
+      throw new Error(
+        "User doesn't exist or has no permissiton to perform this request"
+      );
+    }
+    return this.usersRepository.list();
   }
 }
 
